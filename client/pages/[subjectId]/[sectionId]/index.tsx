0bd826db93f86a460b6c44026d6fb2988b1a1b83
skullcutter02 <=>
2022-02-11 22:13:03 +0800

    Created section page with markdown

--- client/pages/[subjectId]/[sectionId]/index.tsx
+++ client/pages/[subjectId]/[sectionId]/index.tsx
@@ -1,11 +1,47 @@
 import React from "react";
 import { GetStaticPaths, GetStaticProps } from "next";
-import { dehydrate, QueryClient } from "react-query";
+import { dehydrate, QueryClient, useQuery } from "react-query";
+import { useRouter } from "next/router";
+import ReactMarkdown from "react-markdown";
+import ChakraUIRenderer from "chakra-ui-markdown-renderer";
+
 import getSubjectsWithSections from "../../../queries/getSubjectsWithSections";
 import getSection from "../../../queries/getSection";
+import IResponse from "../../../types/response.interface";
+import IResponseData from "../../../types/responseData.interface";
+import ISection from "../../../types/section.interface";
+import MainContainer from "../../../components/ui/MainContainer";
+import { Image } from "@chakra-ui/react";
+
+type Data = IResponse<IResponseData<ISection>>;
+
+const newTheme = {
+  img: (props) => {
+    const { children } = props;
+    return (
+      <Image maxH={"500px"} my={7} {...props}>
+        {children}
+      </Image>
+    );
+  },
+};
 
 const SectionPage: React.FC = () => {
-  return <></>;
+  const sectionId = useRouter().query.sectionId as string;
+
+  const {
+    data: { data },
+  } = useQuery<Data>(["section", sectionId], () => getSection(sectionId));
+
+  return (
+    <>
+      <MainContainer>
+        {data.attributes?.body && (
+          <ReactMarkdown components={ChakraUIRenderer(newTheme)} children={data.attributes.body} />
+        )}
+      </MainContainer>
+    </>
+  );
 };
 
 export const getStaticProps: GetStaticProps = async (context) => {
