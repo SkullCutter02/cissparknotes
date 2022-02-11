bba365596cede3d32e63e075ecd4802921ea9227
skullcutter02 <=>
2022-02-11 18:26:05 +0800

    Added image interface

---
+++ client/types/image.interface.ts
@@ -0,0 +1,40 @@
+export default interface Image {
+  data: {
+    id: number;
+    attributes: {
+      name: string;
+      alternativeText: string;
+      caption: string;
+      width: number;
+      height: number;
+      formats: {
+        thumbnail: Format;
+        large: Format;
+        medium: Format;
+        small: Format;
+      };
+      hash: string;
+      ext: string;
+      mime: string;
+      size: number;
+      url: string;
+      previewUrl: string | null;
+      provider: string;
+      provider_metadata: string;
+      createdAt: string;
+      updatedAt: string;
+    };
+  };
+}
+
+interface Format {
+  name: string;
+  hash: string;
+  ext: string;
+  mime: string;
+  width: number;
+  height: number;
+  size: number;
+  path: string | null;
+  url: string;
+}
