4bab51b6674bcf5ab30c58c3286188058f49eecf
skullcutter02 <=>
2022-02-13 19:41:24 +0800

    Made text so it doesn't take up full width

--- client/components/ui/subject/Subject.tsx
+++ client/components/ui/subject/Subject.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Box, Image, Text } from "@chakra-ui/react";
+import { Box, VStack, Image, Text } from "@chakra-ui/react";
 import Link from "next/link";
 
 import IResponseData from "../../../types/responseData.interface";
@@ -14,23 +14,24 @@ const Subject: React.FC<Props> = ({ subjectData: { id: subjectId, attributes: su
     <>
       <Box borderWidth={"1px"} borderRadius={"lg"} w={"100%"} overflow={"hidden"} p={4}>
         <Image src={subject.image} w={"100%"} h={{ base: "240px", sm: "350px" }} objectFit={"cover"} />
-        <Link href={`/${subjectId}`}>
-          <Text
-            textAlign={"center"}
-            mt={4}
-            fontSize={{ base: 17, md: 20 }}
-            display={"block"}
-            cursor={"pointer"}
-            _hover={{
-              textDecoration: "underline",
-            }}
-          >
-            《{subject.name}》
+        <VStack spacing={1}>
+          <Link href={`/${subjectId}`}>
+            <Text
+              mt={4}
+              maxW={"max-content"}
+              fontSize={{ base: 17, md: 20 }}
+              cursor={"pointer"}
+              _hover={{
+                textDecoration: "underline",
+              }}
+            >
+              《{subject.name}》
+            </Text>
+          </Link>
+          <Text color={"gray.600"} fontSize={{ base: 14, md: 17 }}>
+            {subject.author}
           </Text>
-        </Link>
-        <Text textAlign={"center"} mt={1} color={"gray.600"} fontSize={{ base: 14, md: 17 }}>
-          {subject.author}
-        </Text>
+        </VStack>
       </Box>
     </>
   );
